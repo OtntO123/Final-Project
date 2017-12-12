1b070ddf30aafa4488551233375fe19710910e9c
OtntO123 <31671414+OtntO123@users.noreply.github.com>
2017-12-12 00:04:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 
 if(0) {
 	echo "METHOD ";
@@ -27,7 +27,7 @@ include_once "autoload.php";
 include_once "database.php";
 
 //this starts the program as a static.  Start tracing the program from here following the classes and methods being called
-$response = http\processRequest::createResponse();
+//$response = http\processRequest::createResponse();
 //read the notes below
 
 
